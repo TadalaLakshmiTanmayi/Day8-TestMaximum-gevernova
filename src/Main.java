66b1a0fea3ce692091b4deb8698168675cbb837a
Tanmayi tadala <tanmayitadala@gmail.com>
2024-12-10 22:54:36 +0530

    UC3:To find Maximum among 3 Strings

--- src/Main.java
+++ src/Main.java
@@ -4,34 +4,34 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter the first number: ");
-        float num1 = sc.nextFloat();
+        System.out.print("Enter the first string: ");
+        String str1 = sc.nextLine();
 
-        System.out.print("Enter the second number: ");
-        float num2 = sc.nextFloat();
+        System.out.print("Enter the second string: ");
+        String str2 = sc.nextLine();
 
-        System.out.print("Enter the third number: ");
-        float num3 = sc.nextFloat();
+        System.out.print("Enter the third string: ");
+        String str3 = sc.nextLine();
 
-        MaxNumber maxObj = new MaxNumber();
-        float maxi = maxObj.testMax(num1, num2, num3);
-        System.out.println("Maximum number is " + maxi);
+        MaxString maxObj = new MaxString();
+        String maxi = maxObj.testMax(str1, str2, str3);
+        System.out.println("Maximum string is " + maxi);
         sc.close();
     }
 }
 
-class MaxNumber {
-    public float testMax(float num1, float num2, float num3) {
-        Float maxx = Float.valueOf(num1);
+class MaxString {
+    public String testMax(String str1, String str2, String str3) {
+        String maxStr = str1;
 
-        if (Float.valueOf(num2).compareTo(maxx) > 0) {
-            maxx = Float.valueOf(num2);
+        if (str2.compareTo(maxStr) > 0) {
+            maxStr = str2;
         }
 
-        if (Float.valueOf(num3).compareTo(maxx) > 0) {
-            maxx = Float.valueOf(num3);
+        if (str3.compareTo(maxStr) > 0) {
+            maxStr = str3;
         }
 
-        return maxx.floatValue();
+        return maxStr;
     }
 }
