831b870a616cbb820e917a37b04f0782624d6cd5
Tanmayi tadala <tanmayitadala@gmail.com>
2024-12-10 23:33:01 +0530

    UC5 Commit

--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,17 @@ public class Main {
         System.out.print("Enter 3 strings: ");
         String s1 = sc.next(), s2 = sc.next(), s3 = sc.next();
         MaxValue<String> stringMax = new MaxValue<>(s1, s2, s3);
-        System.out.println("Max String: " + stringMax.testMaximum());
+        stringMax.testMaximum();
 
         System.out.print("Enter 3 integers: ");
         int n1 = sc.nextInt(), n2 = sc.nextInt(), n3 = sc.nextInt();
         MaxValue<Integer> intMax = new MaxValue<>(n1, n2, n3);
-        System.out.println("Max Integer: " + intMax.testMaximum());
+        intMax.testMaximum();
 
         System.out.print("Enter 3 floats: ");
         float f1 = sc.nextFloat(), f2 = sc.nextFloat(), f3 = sc.nextFloat();
         MaxValue<Float> floatMax = new MaxValue<>(f1, f2, f3);
-        System.out.println("Max Float: " + floatMax.testMaximum());
+        floatMax.testMaximum();
 
         sc.close();
     }
@@ -32,12 +32,17 @@ class MaxValue<T extends Comparable<T>> {
         this.values = values;
     }
 
-    public T testMaximum() {
-        return MaxValue.testMaximum(values);
+    public void testMaximum() {
+        T max = MaxValue.testMaximum(values);
+        MaxValue.printMax(max);  // Calling printMax to print the maximum value
     }
 
     public static <T extends Comparable<T>> T testMaximum(T... values) {
         Arrays.sort(values);
-        return values[values.length - 1];
+        return values[values.length - 1]; // After sorting, the largest value will be the last element
+    }
+
+    public static <T> void printMax(T max) {
+        System.out.println("Max Value: " + max);
     }
 }
